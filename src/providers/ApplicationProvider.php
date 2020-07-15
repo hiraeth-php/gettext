b41158970e1947510e91e27188c5a526bbbb5842
Matthew J. Sahagian <matthew.sahagian@gmail.com>
2020-07-15 12:27:04 -0700

    Some fixups

--- src/providers/ApplicationProvider.php
+++ src/providers/ApplicationProvider.php
@@ -25,14 +25,15 @@ class ApplicationProvider implements Hiraeth\Provider
 	 */
 	public function __invoke(object $instance, Hiraeth\Application $app): object
 	{
-		foreach ($app->getConfig('*', 'gettext.domains.locals', []) as $domains) {
+		foreach ($app->getConfig('*', 'gettext.domains.locales', []) as $domains) {
 			foreach ($domains as $domain => $path) {
-				bindtextdomain($domain, $app->getDirectory($path));
+				bindtextdomain($domain, $app->getDirectory($path)->getPathname());
 				bind_textdomain_codeset($domain, 'UTF-8');
 			}
 		}
 
 		textdomain($app->getConfig('packages/gettext', 'gettext.domains.default', 'app'));
+		setlocale(LC_ALL, ...explode(',', $app->getEnvironment('LOCALE', 'en_US.utf8')));
 
 		return $instance;
 	}
